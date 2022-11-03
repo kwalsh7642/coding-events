895c82d1915d9dfb241900470043d7a2af59aa79
Katie Walsh <kwalsh7642@gmail.com>
2022-11-03 01:43:39 -0500

    made a postmapping to retrieve user generated event creation

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,13 +3,18 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping ("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
@@ -21,6 +26,18 @@ public class EventController {
         model.addAttribute("events" , events);
 
         return "events/index";
+
+    }
+
+    @GetMapping("create") //lives at /events/create
+    public String renderCreateEventForm (){
+        return "events/create"; //returns static events form
+    }
+
+    @PostMapping //this method will add a user event to our list
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; // redirects to the root path controller, which is "/events"
 
     }
 }
