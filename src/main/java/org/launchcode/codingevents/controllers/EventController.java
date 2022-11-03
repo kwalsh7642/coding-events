e58c6bb1dd4bbdff15f84346e2e14fc528242c06
Katie Walsh <kwalsh7642@gmail.com>
2022-11-03 13:55:25 -0500

    added properties to model class

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -30,8 +30,9 @@ public class EventController {
     }
 
     @PostMapping ("create") //this method will add a user event to our list
-    public String createEvent(@RequestParam String eventName) {
-        events.add(new Event(eventName));//create a new event object and pass it into the list
+    public String createEvent(@RequestParam String eventName,
+                              @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));//create a new event object and pass it into the list
         return "redirect:"; // redirects to the root path controller, which is "/events"
 
     }
